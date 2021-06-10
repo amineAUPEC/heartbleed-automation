8780dca60f2bff141abedfd4f540325c5fbf1dd7
amineAUPEC <79785242+amineAUPEC@users.noreply.github.com>
2021-06-10 18:21:31 +0200

    1821

--- bash/msf/metasploit.sh
+++ bash/msf/metasploit.sh
@@ -2,7 +2,7 @@
 
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
   chmod 755 msfinstall && \
-  ./msfinstall
+  ./msfinstall 
 
 
 
