f7ec215b11a7ba337007266d1f5ef0b172ff634d
amineAUPEC <79785242+amineAUPEC@users.noreply.github.com>
2022-05-17 23:09:35 +0200

    sudo vulnbank xss docker

--- bash/roscelin-obsolete/xss-vulnerable-server.sh
+++ bash/roscelin-obsolete/xss-vulnerable-server.sh
@@ -6,6 +6,6 @@ git clone https://github.com/amineAUPEC/heartbleed-automation.git
 cd heartbleed-automation/bash
 # sudo chmod +x ./install-docker.sh && sudo ./install-docker.sh && sudo apt-get install -y docker-compose
 sudo chmod +x ./install-docker.sh && sudo ./install-docker.sh 
-docker run --name vulnbank -p 80:80 -d vulnbank/vulnbank
+sudo docker run --name vulnbank -p 80:80 -d vulnbank/vulnbank
 
 # git clone https://github.com/vulnbank/vulnbank.git && cd vulnbank && sudo docker build . -f Dockerfile
